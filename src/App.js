844a8d907eaeb18e2383f74b33368d390aced336
Anatoli <anatoli.railean@gmail.com>
2022-07-02 22:56:54 -0700

    Change contract address

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import {ethers} from "ethers";
 import VaccinePassport from './artifacts/contracts/VaccinePassport.sol/VaccinePassport.json';
 
-const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const greeterAddress = "0x3530C1db973F93Fd93e61D4547485C0e3A236f32";
 
 /**
  * @description This is the main component of the application.
