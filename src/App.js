9fca8ad90d45b6ef6f314e4326b6e9ce8492a7e6
Anatoli <anatoli.railean@gmail.com>
2022-07-06 22:17:00 -0700

    Refactor App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import {ethers} from "ethers";
 import VaccinePassport from './VaccinePassport.json';
 import {Button, Input, Alert, Spinner} from 'reactstrap';
 import VaccineImage from './vaccine.png';
+import PersonDetails from "./views/People/components/PersonDetails";
 
-const contractAddress = "0x3530C1db973F93Fd93e61D4547485C0e3A236f32";
+const contractAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
 
 /**
  * @description This is the main component of the application.
@@ -13,112 +14,91 @@ const contractAddress = "0x3530C1db973F93Fd93e61D4547485C0e3A236f32";
  */
 function App() {
 
-  const [vaccineName, setVaccineName] = useState("");
+  const [personId, setPersonId] = useState("");
+  const [person, setPerson] = useState(null);
+
   const [isLoading, setIsLoading] = useState(false);
 
-  const requestAccounts = async () => {
-    await window?.ethereum?.request({method: "eth_requestAccounts"});
-  }
+  // const requestAccounts = async () => {
+  //   await window?.ethereum?.request({method: "eth_requestAccounts"});
+  // }
 
-  /**
-   * Fetches the latest vaccine name from the smart contract.
-   */
-  const fetchVaccineName = async () => {
+  // /**
+  //  * Fetches the latest vaccine name from the smart contract.
+  //  */
+  // const fetchVaccineName = async () => {
+  //
+  //   if (typeof window?.ethereum !== "undefined") {
+  //     setIsLoading(true);
+  //
+  //     const provider = new ethers.providers.Web3Provider(window.ethereum);
+  //     const contract = new ethers.Contract(contractAddress, VaccinePassport.abi, provider);
+  //
+  //     try {
+  //       const data = await contract.getVaccineName();
+  //
+  //       console.log(data);
+  //
+  //       setVaccineName(data);
+  //       setIsLoading(false);
+  //     } catch (e) {
+  //       console.error(e);
+  //     }
+  //   }
+  // }
+  //
+  // /**
+  //  * Sets the new vaccine name on the smart contract.
+  //  */
+  // const updateVaccineName = async () => {
+  //   if (!vaccineName) return;
+  //
+  //   if (typeof window?.ethereum !== "undefined") {
+  //
+  //     try {
+  //
+  //       setIsLoading(true);
+  //
+  //       await requestAccounts();
+  //
+  //       const provider = new ethers.providers.Web3Provider(window.ethereum);
+  //       const signer = provider.getSigner();
+  //
+  //       const contract = new ethers.Contract(contractAddress, VaccinePassport.abi, signer);
+  //       const transaction = await contract.setVaccineName(vaccineName);
+  //
+  //       setVaccineName("");
+  //       await transaction.wait();
+  //       await fetchVaccineName();
+  //
+  //     } catch (e) {
+  //       console.error(e);
+  //     } finally {
+  //       setIsLoading(false);
+  //     }
+  //
+  //   }
+  // }
 
-    if (typeof window?.ethereum !== "undefined") {
-      setIsLoading(true);
+  const getPerson = async () => {
 
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const contract = new ethers.Contract(contractAddress, VaccinePassport.abi, provider);
-
-      try {
-        const data = await contract.getVaccineName();
-
-        console.log(data);
-
-        setVaccineName(data);
-        setIsLoading(false);
-      } catch (e) {
-        console.error(e);
-      }
-    }
-  }
-
-  /**
-   * Sets the new vaccine name on the smart contract.
-   */
-  const updateVaccineName = async () => {
-    if (!vaccineName) return;
-
-    if (typeof window?.ethereum !== "undefined") {
-
-      try {
-
-        setIsLoading(true);
-
-        await requestAccounts();
-
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-        const signer = provider.getSigner();
-
-        const contract = new ethers.Contract(contractAddress, VaccinePassport.abi, signer);
-        const transaction = await contract.setVaccineName(vaccineName);
-
-        setVaccineName("");
-        await transaction.wait();
-        await fetchVaccineName();
-
-      } catch (e) {
-        console.error(e);
-      } finally {
-        setIsLoading(false);
-      }
-
-    }
   }
 
   return (
-    <div className="App" style={{position: 'absolute', textAlign: "center", left: "40%", top: "20%", width: "30vw"}}>
-      <img
-        src={VaccineImage}
-        alt="car"
-      />
+    <div className="App" style={{position: 'absolute', textAlign: "center", left: "35%", top: "20%", width: "30vw"}}>
+      <img src={VaccineImage} alt="car"/>
       <h1>Vaccine Passport</h1>
       <p>Welcome to the blockchain powered web application</p>
-      <br/>
-      {
-        vaccineName &&
-        <Alert
-          color="primary"
-        >
-          <p>Latest Vaccine Name Saved on Blockchain: <strong>{vaccineName}</strong></p>
-        </Alert>
-      }
-      <Button color="primary" onClick={fetchVaccineName}>Get Vaccine Name</Button>
-      <br/>
-
-      <br/>
       <Input
-        type="text"
-        value={vaccineName}
-        onChange={(event) => setVaccineName(event.target.value)}
-        placeholder="Enter your vaccine name"
+        value={personId}
+        style={{marginTop: "2em", marginBottom: "2em"}}
+        onChange={(e) => setPersonId(e.target.value)}
+        placeholder="Person ID"
       />
-      <br/>
-      <Button color="success" onClick={updateVaccineName}>Save to Blockchain</Button>
-      <div style={{marginTop: "3em"}}>
-        {
-          isLoading &&
-          <Spinner
-            color="primary"
-            type="grow"
-            size="lg"
-          >
-            Loading...
-          </Spinner>
-        }
-      </div>
-
+      <Button color="primary" onClick={() => getPerson()}>Search</Button>
+      {
+        person && !isLoading && <PersonDetails person={person}/>
+      }
     </div>
   );
 }
